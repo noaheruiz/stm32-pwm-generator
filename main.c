e523bac8e544f143a73b23b4cb5cf8f044d0dc55
Noah <noahmvp2018@icloud.com>
2026-06-22 12:46:04 -0500

    Added ST license

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/*
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+*/
+
 #include "main.h"
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_tim.h"
